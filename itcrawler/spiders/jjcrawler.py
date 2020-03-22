92d26d2b689476835ea741941f36e2d764622403
MMarciniak103 <michal.marciniak@interia.eu>
2020-03-22 12:53:58 +0100

    Updated version

--- itcrawler/spiders/jjcrawler.py
+++ itcrawler/spiders/jjcrawler.py
@@ -8,6 +8,10 @@ import time
 class JjcrawlerSpider(scrapy.Spider):
 		name = 'jjcrawler'
 		SCROLL_PAUSE_TIME = 0.5
+		TABLE_LAYOUT = "//div[@class='css-1macblb']" #must be keyborard reachable
+		ITEM_SELECTOR = ".//a[@class='css-18rtd1e']/div[@class='css-xhufe5']" #Single item in offer list
+		UPPER_ROW = "div[@class='css-qjm23f']" #Upper row containing offer name and price
+		BOTTOM_ROW = "div[@class='css-mih8cb']" #Bottom row containing company name, city and tags
 
 		def start_requests(self): 
 			yield SeleniumRequest(
@@ -30,7 +34,8 @@ class JjcrawlerSpider(scrapy.Spider):
 			driver = response.meta['driver']
 			driver.maximize_window()
 			
-			search_input = driver.find_element_by_xpath("//div[@class='css-40k5xv']")
+
+			search_input = driver.find_element_by_xpath(self.TABLE_LAYOUT)
 												
 
 			last_height = driver.execute_script("return arguments[0].scrollHeight",search_input)
@@ -43,7 +48,7 @@ class JjcrawlerSpider(scrapy.Spider):
 
 			response_obj = self.fetch_html(driver)
 			
-			offer_list = response_obj.xpath("//div[@class='css-40k5xv']/div")
+			offer_list = response_obj.xpath(f"{self.TABLE_LAYOUT}/div")
 			
 			last_element = offer_list.xpath(".//div[starts-with(@style,'position:')]")[-1]
 			last_element_top_position = self.parse_position(last_element.xpath(".//@style").get())
@@ -51,22 +56,31 @@ class JjcrawlerSpider(scrapy.Spider):
 			#Get back to top
 			driver.execute_script("arguments[0].scrollTo(0,0)",search_input)
 
+			time.sleep(self.SCROLL_PAUSE_TIME)
+
 			last_top_position_seen = -1
 
-			print(last_element_top_position)
-
+			#----------------------- SCROLL THROUGH THE PAGE UNTIL YOU REACH THE BOTTOM -------------------
+			#check if last visible item's top position is the same as overall last item's position
 			while last_top_position_seen < last_element_top_position:
 				response_obj = self.fetch_html(driver)
-				offer_list = response_obj.xpath("//div[@class='css-40k5xv']/div")
-			
+				offer_list = response_obj.xpath(f"{self.TABLE_LAYOUT}/div")
+				
 				for item in offer_list.xpath(".//div[starts-with(@style,'position:')]"):
 					item_pos = self.parse_position(item.xpath(".//@style").get())
+					keywords = []
+					for keyword in item.xpath(f"{self.ITEM_SELECTOR}/{self.BOTTOM_ROW}/div[@class='css-1ij7669']/div"):
+						keywords.append(keyword.xpath("normalize-space(.//text())").get())
+					#If your current item position was seen in previous iteration -> ignore it
 					if item_pos <= last_top_position_seen:
 						continue
 					else:
 						yield{
-						'title':item.xpath("normalize-space(.//a[@class='css-18rtd1e']/div[@class='css-1djzjwc']/div[@class='css-4vtr3h']/div[@class='css-dcciyt']/div[@class='css-rm26dz']/text())").get(),
-						'top position':item_pos
+						'title':item.xpath(f"normalize-space({self.ITEM_SELECTOR}/{self.UPPER_ROW}/div[@class='css-18hez3m']/div[@class='css-wjfk7i']/text())").get(),
+						'price range':item.xpath(f"normalize-space({self.ITEM_SELECTOR}/{self.UPPER_ROW}/div[@class='css-v6uxww']/span/text())").get(),
+						'company':item.xpath(f"normalize-space({self.ITEM_SELECTOR}/{self.BOTTOM_ROW}/div[@class='css-pdwro7']/div[@class='css-ajz12e']/text())").get(),
+						'city':item.xpath(f"normalize-space({self.ITEM_SELECTOR}/{self.BOTTOM_ROW}/div[@class='css-pdwro7']/div[@class='css-1n50ecq']/text())").get(),
+						'keywords':keywords
 						}
 
 
