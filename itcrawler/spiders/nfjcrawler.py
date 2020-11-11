305930e1a31be9e71b2fcd9e5b9ade40e4fdb578
Michal <m.marciniak@neurosys.com>
2020-11-11 19:47:37 +0100

    Update nfjcrawler for different page structure

--- itcrawler/spiders/nfjcrawler.py
+++ itcrawler/spiders/nfjcrawler.py
@@ -46,11 +46,11 @@ class NfjcrawlerSpider(scrapy.Spider):
 
 				for item in  content_wrapper.xpath(".//a[contains(@class,'posting-list-item')]"):
 					yield {
-					'title':item.xpath(f"normalize-space({self.FIRST_COL}/h4/text())").get(),
+					'title':item.xpath(f"normalize-space({self.FIRST_COL}/h3/text())").get(),
 					'price range': ''.join(item.xpath(f"{self.SECOND_COL}/nfj-posting-item-tags/span[@class='text-truncate badgy salary btn btn-outline-secondary btn-sm']/text()").extract()),
 					'company':item.xpath(f"normalize-space({self.FIRST_COL}/span/text())").get().replace('w ','',1),
 					'city':item.xpath(f"normalize-space({self.SECOND_COL}/span[@class='posting-info__location d-flex align-items-center ml-auto']/nfj-posting-item-city/text())").get(),
-					'keywords':''.join(item.xpath("./a/div[@class='posting-info position-relative d-none d-lg-flex flex-grow-1']/nfj-posting-item-tags[@class='ml-3']/nfj-posting-item-tag/object/a/text()").extract()),
+					'keywords':''.join(item.xpath(".//div[@class='posting-info position-relative d-none d-lg-flex flex-grow-1']/nfj-posting-item-tags[@class='ml-3']/nfj-posting-item-tag/object/a/text()").extract()),
 					'url':'nofluffjobs.com'+item.xpath('./@href').get()
 					}
 				
